59b79659245eb53ccc533e97d36786d7378d3ad6
1d8 <56985822+1d8@users.noreply.github.com>
2022-12-02 13:41:12 -0800

    Update servicemon.go

--- servicemon/servicemon.go
+++ servicemon/servicemon.go
@@ -9,6 +9,8 @@ import (
 	"io/ioutil"
 	"encoding/hex"
 	"crypto/sha1"
+	"go.uber.org/zap"
+	"github.com/xFaraday/gomomento/alertmon"
 )
 
 type ServiceStats struct {
@@ -38,7 +40,6 @@ func ListServices() []ServiceStats {
 // Take service snapshot
 // Sleep for specified amount of time
 // Take another service snapshot & compare against previous one to check for changes
-// Output changes (TODO)
 // Call ServiceMonitor(), it'll handle the rest
  func ServiceMonitor(sleepDuration time.Duration) {
 	serviceSnapHashOrig := ServiceSnap()
@@ -46,17 +47,30 @@ func ListServices() []ServiceStats {
 		if len(serviceSnapHashOrig) != 0 {
 			time.Sleep(sleepDuration * time.Second)
 			serviceSnapHashNew := ServiceSnap()
-			fmt.Println(serviceSnapHashOrig)
-			fmt.Println(serviceSnapHashNew)
-			fmt.Println("[+] Checking hashes...")
+			//fmt.Println("[+] Checking hashes...")
 			if serviceSnapHashOrig != serviceSnapHashNew {
-				fmt.Println("[!] Hashes for service files do not match!")
+				//fmt.Println("[!] Hashes for service files do not match!")
+				//func GetDifference(fileInput1 string, fileInput2 string) string {
+				diff := GetDifference("/tmp/servicesnap.orig", "/tmp/servicesnap.duplicate")
+				zlog := zap.S().With(
+					"REASON": "Service snapshots do not match! Potential tampering with services!",
+					"Diff output": diff,
+				)
+				user, _ := exec.Command("/usr/bin/whoami").Output()
+				var inc alertmon.Incident = alertmon.Incident{
+					Name:	"Service Snapshot Mismatch",
+					User:	string(user),
+					Process: "",
+					RemoteIP: "",
+					Cmd: "",
+				}
 			} else {
 				fmt.Println("[+] Hashes match. Resuming rest...")
 			}
 		}
 	}
 }
+
 
 
 // create service file, return hash of it
@@ -67,7 +81,7 @@ func CreateServiceFile(serviceFileName string) string {
 		} else {
 			serviceSnap := ListServices()
 			for _, service := range serviceSnap {
-				fileHandle.Write([]byte(service.serviceName))
+				fileHandle.Write([]byte(service.serviceName + " " + service.serviceStatus))
 				fileHandle.Write([]byte("\n"))
 			}
 		}
@@ -91,7 +105,7 @@ func ServiceSnap() string {
 	// create the service snap file if it doesn't already exist
 	if os.IsNotExist(err) {
 		fmt.Println("[+] Original service file doesn't exist. Creating...")
-		CreateServiceFile(serviceFileName string) 
+		CreateServiceFile(serviceSnapshotFile) 
 		serviceDataHash := CreateServiceFile(serviceSnapshotFile)
 		return serviceDataHash
 	} else {
@@ -102,3 +116,11 @@ func ServiceSnap() string {
 	}
 }
 
+func GetDifference(fileInput1 string, fileInput2 string) string {
+	// diff cmd is error'ing out here
+	diffOut, err := exec.Command("/usr/bin/diff", fileInput1, fileInput2).Output()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return string(diffOut)
+}
